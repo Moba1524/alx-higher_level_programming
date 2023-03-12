2e0d32f4391337e40ee0f6fd48f59f22b74cc536
Mothafar Bashir <mothafr2016@gmail.com>
2023-03-12 01:19:04 -0800

    Tasks 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-    string_without_c = ""
-
-    for c in my_string:
-        if c != 'c' and c != 'C':
-            string_without_c += c
-
-        return(string_without_c)
+    my_string_list = list(my_string)
+    index_count = 0
+    for index in my_string_list:
+        if index == 'c' or index == 'C':
+            my_string_list[index_count]
+        index_count += 1
+    return "".join(my_string_list)
