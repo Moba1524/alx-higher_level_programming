75f957daf5b5adfbb1e71a99593fc9950f19fbaf
Mothafar Bashir <mothafr2016@gmail.com>
2023-04-12 17:54:31 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""0x0B. Python - Input/Output, task 0. Read file """
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """Reads contents of a text file and print to stdout.
+    Args:
+        filename (str): name of file to be opened
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
